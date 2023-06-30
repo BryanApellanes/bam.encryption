a047a5490a1104f606825970dbc4d2f43b9c8558
Bryan Apellanes <bryan@threeheadz.com>
2023-06-29 19:43:02 -0500

    it compiles!

---
+++ bam.encryption/bam.encryption/Encryption/EncryptionDataRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bam.Net.Data;
+using Bam.Net.Encryption.Data;
+
+namespace Bam.Net.Encryption
+{
+    public class EncryptionDataRepository
+    {
+        public Database Database { get; internal set; }
+
+        internal IClientKeySet GetClientKeySet(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IServerKeySet GetServerKeySet(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task<IClientKeySet> SaveAsync(IClientKeySet copy)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task<IServerKeySet> SaveAsync(IServerKeySet copy) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ bam.encryption/bam.encryption/Encryption/IServerKeySource.cs
@@ -0,0 +1,11 @@
+﻿using Bam.Net.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bam.net.shared.Encryption
+{
+    public interface IServerKeySource : IAesKeySource, IRsaKeySource
+    {
+    }
+}
